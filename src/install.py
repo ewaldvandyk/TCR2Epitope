ea52a3c36ed6cd07bb31011992c9018f207c07b1
ewaldvandyk <ewald.redmage@gmail.com>
2017-12-07 15:58:58 +0100

    install file complete for compiling c code

--- src/install.py
+++ src/install.py
@@ -1,11 +1,13 @@
 import os
+import subprocess
 
 # c++ gsl include paths. Only add if gsl includes are not located in the search path of gcc
-gsl_include_path = None 
-gsl_lib_path     = None
-# gsl_include_path = os.path.join("/", "usr", "local", "include") #Default for OSX installation
-# gsl_include_path = os.path.join("/", "usr", "include") #Default for Linux installation
-# gsl_lib_path = os.path.join("/", "usr", "local", "lib") #Default for OSX
+gsl_include_path = None # Set to None if gsl_include_path is already visible to gcc 
+gsl_lib_path     = None # Set to None if gsl_lib_path is already visible to gcc
+# gsl_include_path = "/Users/ewaldvandyk/bin/gsl/include" 
+# gsl_lib_path = "/Users/ewaldvandyk/bin/gsl/lib"
+
+gcc_bin_name = "gcc"
 
 
 # Path directories
@@ -18,6 +20,23 @@ gmm_c_file  = os.path.join(data_transform_path, 'gmm.c')
 gmm_o_file  = os.path.join(data_transform_path, 'gmm.o')
 gmm_so_file = os.path.join(data_transform_path, 'gmm.so')
 
+if gsl_include_path == None:
+    compile_args = [gcc_bin_name, "-c", "-fPIC", gmm_c_file, "-o", gmm_o_file]
+else:
+    compile_args = [gcc_bin_name, "-c", "-fPIC", "-I", gsl_include_path, gmm_c_file, "-o", gmm_o_file]
+
+if gsl_lib_path == None:
+    lib_gsl_arg = "-lgsl"
+    lib_cblas_arg = "-lgslcblas"
+else:
+    lib_gsl_arg     = os.path.join(gsl_lib_path, "libgsl.a")
+    lib_cblas_arg   = os.path.join(gsl_lib_path, "libgslcblas.a")
+link_args = [gcc_bin_name, "-shared", gmm_o_file, lib_gsl_arg, lib_cblas_arg, "-o", gmm_so_file]
+
+proc = subprocess.Popen(compile_args)
+proc.wait()
+proc = subprocess.Popen(link_args)
+proc.wait()
 
 
 print gsl_include_path
